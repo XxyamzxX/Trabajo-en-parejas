27e009a940cd7010531263535980fe84f49ea140
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-03-01 14:26:30 -0500

    COnfiguración de la suma

--- PAREJAS.cpp
+++ PAREJAS.cpp
@@ -5,7 +5,7 @@
 int main(){
 
 int opcion;
-
+float num1, num2, resultado;
 std::cout<<"::::::::::  MEN�  ::::::::::"<<std::endl;
 std::cout<<""<<std::endl;
 std::cout<<"\t 1.SUMA"<<std::endl;
@@ -14,6 +14,28 @@ std::cout<<"\t 2.RESTA \n \t 3.PRODUCTO \n \t 4.DIVISI�N "<<std::endl;
 
 std::cin>>opcion;
 std::cout<<"La opci�n elegida es: "<<opcion<<std::endl;
+
+
+if (opcion==1)
+{
+	
+	/* system ("clear") */
+
+	resultado=num1+num2;
+	
+	std::cout<<"La suma de "<<num1<<" y "<<num2<<" Es igual a: "<<resultado<<std::endl;
+	
+		
+	
+	
+	
+	/* system ("clear") */
+
+
+
+
+
+}
 
 
 
