ffb20619575110c42a426a3b52d5528c9b9f9e09
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-03-01 14:21:25 -0500

    Creación del menú

--- PAREJAS.cpp
+++ PAREJAS.cpp
@@ -1,1 +1,22 @@
 //creación del archivo//
+#include <iostream>
+
+
+int main(){
+
+int opcion;
+
+std::cout<<"::::::::::  MEN�  ::::::::::"<<std::endl;
+std::cout<<""<<std::endl;
+std::cout<<"\t 1.SUMA"<<std::endl;
+std::cout<<"\t 2.RESTA \n \t 3.PRODUCTO \n \t 4.DIVISI�N "<<std::endl;
+
+
+std::cin>>opcion;
+std::cout<<"La opci�n elegida es: "<<opcion<<std::endl;
+
+
+
+
+
+}|
